c18e3a82722d9a89ae24b93c723b2c05d6d8a690
Pho3nix24 <sra55tvm@gmail.com>
2020-05-10 10:50:10 +0530

    Logo Changed and added more data

--- lib/views/loginview.dart
+++ lib/views/loginview.dart
@@ -4,7 +4,8 @@ class LoginView extends StatelessWidget {
   var data = [
     {"name": "Antony", "age": 22},
     {"name": "Ananthu", "age": 28},
-    {"name": "Anzar", "age": 21}
+    {"name": "Anzar", "age": 21},
+    {"name": "Sravan", "age": 21},
   ];
 
   @override
@@ -20,7 +21,7 @@ class LoginView extends StatelessWidget {
                   Card(
                     elevation: 5.0,
                     child: ListTile(
-                    leading: Icon(Icons.perm_identity),
+                    leading: Icon(Icons.android),
                     trailing: int.parse(data[index]["age"].toString())>25? Icon(Icons.check): Icon(Icons.clear),
                     title: Text("Name: " + data[index]["name"]),
                     subtitle: Text("Age " + data[index]["age"].toString()),
